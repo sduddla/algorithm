3ed907518b24f2c49a90c51e05b3afd45be3fe76
sy <sduddla921@gmail.com>
2025-04-18 00:11:45 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,9 @@
+function solution(numer1, denom1, numer2, denom2) {
+    let numer = numer1*denom2 + numer2*denom1;
+    let denom = denom1 * denom2;
+    
+    let getGcd = (a, b) => (a % b === 0 ? b : getGcd(b, a % b));
+    
+    let gcd = getGcd(numer, denom);
+    return [numer/gcd, denom/gcd ];
+}
