18d63d56e18de46341712941b068bf0a7498dd7d
sy <sduddla921@gmail.com>
2025-05-06 02:28:32 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    const arr = s.split(' ');
+    let answer = 0;
+    
+    arr.forEach((v, i) => {
+        if(v === 'Z') {
+            answer -= Number(arr[i-1]);
+        } else {
+            answer += Number(v);
+        }
+    })
+    return answer;
+}
