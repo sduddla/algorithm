4456eff48390d22a7e90b62708951ffd6207f434
sy <sduddla921@gmail.com>
2025-05-06 02:23:47 +0900

    [level 0] Title: 소인수분해, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let answer = new Set();
+    
+    for(let i=2; i<=n; i++) {
+        while(n % i === 0) {
+            answer.add(i);
+            n /= i;
+        }
+    }
+    return [...answer];
+}
