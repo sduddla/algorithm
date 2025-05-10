cfa999005ba1237c1dea0f0a066d8d214ecd7672
sy <sduddla921@gmail.com>
2025-05-11 01:50:37 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let str = n.toString().split('');
+    let answer = 0;
+    
+    for(let i=0; i<str.length; i++) {
+        answer += parseInt(str[i]);
+    }
+    return answer;
+}
