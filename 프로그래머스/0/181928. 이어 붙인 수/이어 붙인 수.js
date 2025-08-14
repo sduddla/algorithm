62c2bf8c1209f3f19f335c5fbb5fb02128ff1a6f
sy <sduddla921@gmail.com>
2025-08-14 15:14:14 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,17 @@
+function solution(num_list) {
+    let even = [];
+    let odd = [];
+    
+    for(let i=0; i<num_list.length; i++) {
+        if(num_list[i] % 2 === 0) {
+            even.push(num_list[i]);
+        } else {
+            odd.push(num_list[i]);
+        }
+    }
+    
+    const evenNum = Number(even.join(''));
+    const oddNum = Number(odd.join(''));
+    
+    return evenNum + oddNum; 
+}
