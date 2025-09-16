0d0f2ed2ee10a71a8777c2be917dd472befc057f
sy <sduddla921@gmail.com>
2025-09-17 00:46:07 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,20 @@
+function solution(s){
+    s = s.toLowerCase();
+    
+    let pCnt = 0;
+    let yCnt = 0;
+    
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === 'p') {
+            pCnt++;
+        } else if(s[i] === 'y') {
+            yCnt++;
+        }
+    }
+    
+    if(pCnt === yCnt) {
+        return true;
+    } else {
+        return false;
+    }
+}
