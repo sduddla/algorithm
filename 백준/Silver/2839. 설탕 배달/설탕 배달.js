ae2136e852e6579722b7833947ae0be157f928ea
sy <sduddla921@gmail.com>
2025-09-18 00:35:06 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -2,16 +2,15 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim();
 
 let n = Number(input);
-let count = 0;
+let cnt = 0;
 
 while(n >= 0) {
     if(n % 5 === 0) {
-        count += Math.floor(n / 5);
-        console.log(count);
+        cnt += Math.floor(n / 5);
+        console.log(cnt);
         return;
     }
     n -= 3;
-    count++;
+    cnt++;
 }
-
 console.log(-1);
