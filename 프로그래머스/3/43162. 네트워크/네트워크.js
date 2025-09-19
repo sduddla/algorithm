a13e2a3bfb65f703f127bdcef376b29fac75279f
sy <sduddla921@gmail.com>
2025-09-20 00:40:49 +0900

    [level 3] Title: 네트워크, Time: 0.39 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,29 +1,28 @@
 function solution(n, computers) {
+    
     // n - 컴퓨터 개수
     // computers - 연결에 대한 정보가 담긴 2차원 배열
     
     const visited = Array(n).fill(false);
-    let count = 0;
+    let cnt = 0;
+    
     
     function dfs(node) {
         visited[node] = true; // 방문 처리
-        
         for(let next = 0; next < n; next++) { // 현재 컴퓨터와 연결된 다른 컴퓨터들 탐색
-            // 연결되어 있거나
-            // 방문되지 않은 컴퓨터라면
-            if(computers[node][next] === 1 && !visited[next]) {
+            if(!visited[next] && computers[node][next] === 1) { // 방문하지 않았고, 동시에 연결되어 있다면
                 dfs(next); // 연결된 컴퓨터도 방문 - 그와 연결된 모든 컴퓨터까지 쭉 탐색
             }
         }
     }
     
-    // 모든 컴퓨터 돌면서 방문하지 않은 컴퓨터 있으면 DFS 시작
-    for(let i = 0; i < n; i++) {
+    for(let i = 0; i < n; i++) { // 모든 컴퓨터 돌면서 방문하지 않은 컴퓨터 있다면 dfs 시작
         if(!visited[i]) {
-            dfs(i); // i번 컴퓨터를 시작점으로 연결된 전체 네트워크 탐색
-            count++; // 네트워크 하나 완전히 탐색 -> count++
+            dfs(i); // i번 컴퓨터 시작점으로 연결된 전체 네트워크 탐색
+            cnt++; // 네트워크 하나 완전히 탐색 -> 카운트
         }
     }
     
-    return count;
+    return cnt;
+
 }
