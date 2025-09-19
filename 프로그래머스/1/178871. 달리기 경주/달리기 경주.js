30971741daadb1d5d25347eae886c0a83e5d1be6
sy <sduddla921@gmail.com>
2025-09-19 10:30:06 +0900

    [level 1] Title: 달리기 경주, Time: 175.98 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -7,17 +7,17 @@ function solution(players, callings) {
     for(let name of callings) {
         let index = save[name]; // 불린 선수 현재 위치 찾기
         
-        if(index > 0) { // 맨 앞이 아닐 때
-            let prev = players[index - 1]; // 불린 선수 앞 사람 이름
+        let prev = players[index - 1]; // 불린 선수 앞 사람 이름
+        
+        // players 배열에서 자리 바꾸기
+        [players[index - 1], players[index]] = [players[index], players[index - 1]];
             
-            // players 배열에서 자리 바꾸기
-            [players[index - 1], players[index]] = [players[index], players[index - 1]];
-            
-            // 위치 바꾸기
-            save[name] = index  - 1;
-            save[prev] = index;
-        }
+        // 위치 바꾸기  
+        save[name] = index  - 1;    
+        save[prev] = index;    
     }
     
-    return players;
+     return players;
 }
+    
+   
