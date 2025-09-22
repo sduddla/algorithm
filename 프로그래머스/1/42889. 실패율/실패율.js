3d123bef0a6b4bfd16c68f53cc5e2fc98b79793b
sy <sduddla921@gmail.com>
2025-09-22 19:05:58 +0900

    [level 1] Title: 실패율, Time: 4990.80 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,29 +1,29 @@
 function solution(N, stages) {
-    // N - 전체 스테이지의 개수
-    // stages - 게임을 이용하는 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열
-    // 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열 return
+    // N: 전체 스테이지 개수
+    // stages: 게임을 이용하는 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열
     
     // 실패율
-    let fail = [];
+    // 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지 도달한 플레이어 수
+    
+    let result = [];
     
     for(let i = 1; i <= N; i++) {
-        // 도전자 수
+        // 스테이지에 도달한 플레이어 수
         let cNum = stages.filter(stage => stage >= i).length;
         
-        // 실패자 수
+        // 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수
         let fNum = stages.filter(stage => stage === i).length;
         
-        // 실패율 계산
-        let rate = cNum === 0 ? 0 : fNum / cNum;
+        // 실패율
+        let failRate = cNum === 0 ? 0 : fNum / cNum;
         
-        fail.push([i, rate]); // 스테이지 번호, 실패율
+        result.push([i, failRate]); // 스테이지 번호, 실패율
     }
     
-    fail.sort((a, b) => {
-        if(b[1] === a[1]) return a[0] - b[0];
+    result.sort((a, b) => {
+        if(a[1] === b[1]) a[0] - b[0];
         else return b[1] - a[1];
     })
     
-    return fail.map(item => item[0]);
-    
+    return result.map(item => item[0]);
 }
