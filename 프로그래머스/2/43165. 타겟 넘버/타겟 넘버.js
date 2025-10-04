7b62a6827f63816fe851688d2752a86805bd8c2c
sy <sduddla921@gmail.com>
2025-10-04 22:32:18 +0900

    [level 2] Title: 타겟 넘버, Time: 18.68 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,24 +1,17 @@
 function solution(numbers, target) {
     
-    // 사용할 수 있는 숫자가 담긴 배열 - numbers
-    // 타겟 넘버 - target
+    let cnt = 0; // 타겟 넘버 만드는 방법의 수
     
-    // numbers 배열에 + 또는 - 붙여서 계산
-    // 모든 숫자에 대해 더하거나 빼는 모든 경우의 수 고려
-    // 최종합 -> target 되는 경우의 수
-    
-    let cnt = 0;
-    
-    function dfs(index, sum) { // 인덱스, 현재까지의 합
+    function dfs(index, sum) { // 인덱스, 현재까지 합
         if(index === numbers.length) {
             if(sum === target) {
                 cnt++;
             }
             return;
         }
-        
         dfs(index + 1, sum + numbers[index]);
         dfs(index + 1, sum - numbers[index]);
+        
     }
     dfs(0, 0);
     
