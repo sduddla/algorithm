f7bef8cd5c059437cb8d725b25af117209233193
sy <sduddla921@gmail.com>
2025-10-04 18:13:07 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.73 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,49 +1,48 @@
 function solution(new_id) {
-  let s = new_id;
+    let str = new_id;
     
-  // 1. 모든 대문자를 소문자로 변환
-  s = s.toLowerCase();
+    // 1. 소문자로 치환
+    str = str.toLowerCase();
     
-  // 2. 허용된 문자(소문자, 숫자, '-', '_', '.')만 남기기
-  let temp = '';
-  
-  for(let ch of s) {
-    if((ch >= 'a' && ch <= 'z') || 
-       (ch >= '0' && ch <= '9') ||
-       (ch === '-' || ch === '_' || ch === '.')) {
-      temp += ch;  
+    // 2. 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)만 남기기
+    let temp = '';
+    
+    for(let ch of str) {
+        if((ch >= 'a' && ch <= 'z') ||
+           (ch >= '0' && ch <= '9') ||
+           (ch === '-' || ch === '_' || ch === '.')) {
+            temp += ch;
+        }
     }
-  }
-  s = temp;
     
-  // 3. 마침표가 연속되면 하나로 치환
-  temp = '';
-  
-  for(let i = 0; i < s.length; i++) {
+    str = temp;
     
-    // 연속된 마침표가 아니라면
-    if(!(s[i] === '.' && temp[temp.length - 1] === '.')) { 
-      temp += s[i];
+    // 3. 마침표 2번 이상 연속되면 하나로 치환
+    temp = '';
+    
+    for(let i = 0; i < str.length; i++) {
+        if(!(str[i] === '.' && temp[temp.length - 1] === '.')) {
+            temp += str[i]; 
+        }
     }
-  }
     
-  s = temp;
+    str = temp;
     
-  // 4. 마침표가 처음이나 끝에 있으면 제거
-  if(s[0] === '.') s = s.slice(1);
-  if(s[s.length - 1] === '.') s = s.slice(0, -1);
+    // 4. 마침표가 처음이나 끝에 있으면 제거
+    if(str[0] === '.') str = str.slice(1);
+    if(str[str.length - 1] === '.') str = str.slice(0, -1);
     
-  // 5. 빈 문자열이면 'a' 대입
-  if(s === '') s = 'a';
+    // 5. 빈 문자열이면 'a' 대입
+    if(str === '') str = 'a';
     
-  // 6. 길이가 16자 이상이면 15자까지만 자르고, 끝에 마침표가 있으면 제거
-  if(s.length >= 16) s = s.slice(0, 15);
-  if(s[s.length - 1] === '.') s = s.slice(0, -1);
+    // 6. 길이가 16자 이상이면 15자까지 자르고, 끝에 마침표 있으면 제거
+    if(str.length >= 16) str = str.slice(0, 15);
+    if(str[str.length - 1] === '.') str = str.slice(0, -1);
     
-  // 7. 길이가 2자 이하라면, 마지막 문자를 길이가 3 될 때까지 반복
-  while(s.length < 3) {
-    s += s[s.length - 1];
-  }
+    // 7. 길이가 2자 이하라면, 마지막 문자를 길이가 3 될 때까지 반복
+    while(str.length <= 2) {
+        str += str[str.length - 1];
+    }
     
-  return s;    
+    return str;
 }
