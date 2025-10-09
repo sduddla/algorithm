adb1d8e19a7d1e76e7fd34b2a2963cc92cdc2615
sy <sduddla921@gmail.com>
2025-10-09 18:01:53 +0900

    [level 0] Title: 2의 영역, Time: 0.45 ms, Memory: 42.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    let result = [];
+    
+    let start = arr.indexOf(2);
+    let end = arr.lastIndexOf(2);
+    
+    if(start === -1) return [-1];
+    
+    return arr.slice(start, end + 1);
+}
