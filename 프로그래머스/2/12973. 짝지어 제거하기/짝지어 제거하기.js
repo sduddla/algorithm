f261a582618f7b83cc4ef3d86b86c8477c9f3721
sy <sduddla921@gmail.com>
2025-10-13 17:01:52 +0900

    [level 2] Title: 짝지어 제거하기, Time: 330.30 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -3,13 +3,12 @@ function solution(s)
     const stack = [];
     
     for(let c of s) {
-        // 스택이 비어있지 않고, 현재 문자와 스택 맨 위 문자가 같을 경우
-        if(stack.length > 0 && stack[stack.length - 1] === c) {
+        // 스택이 비어 있지 않고, 현재 문자와 스택 맨 위 문자랑 같을 경우
+        if(!(stack.length === 0) && stack[stack.length - 1] === c) {
             stack.pop();
         } else {
             stack.push(c);
         }
     }
-    
     return stack.length === 0 ? 1 : 0;
 }
