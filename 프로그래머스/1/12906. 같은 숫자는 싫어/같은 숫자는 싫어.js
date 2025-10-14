41783f151c4bc70590d0b71da53382c25bda6a6b
sy <sduddla921@gmail.com>
2025-10-14 11:22:55 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 36.90 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,12 +1,11 @@
 function solution(arr)
 {
-    let stack = [];
+    let answer = [];
     
     for(let i = 0; i < arr.length; i++) {
-        if(arr[i] !== arr[i+1]) {
-            stack.push(arr[i]);
+        if(arr[i] !== arr[i + 1]) {
+            answer.push(arr[i]);
         }
     }
-    
-    return stack;
+    return answer;
 }
