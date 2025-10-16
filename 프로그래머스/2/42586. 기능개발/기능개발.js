0811d4c26208eea4451301d2537196707af2b479
sy <sduddla921@gmail.com>
2025-10-16 13:49:22 +0900

    [level 2] Title: 기능개발, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,29 +1,22 @@
 function solution(progresses, speeds) {
-    // progresses - 먼저 배포되어야 하는 순서대로 작업의 진도가 적힌 정수 배열
-    // speeds - 각 작업의 개발 속도가 적힌 정수 배열
-    // 각 배포마다 몇 개의 기능이 배포되는 지 return
+    const answer = [];
+    const n = progresses.length;
     
-    // 각 작업이 얼마나 걸리는 지
-    let days = [];
-    for(let i = 0; i < progresses.length; i++) {
-        let leftPercent = 100 - progresses[i];
-        let day = Math.ceil(leftPercent / speeds[i]);
-        days.push(day);
-    }
+    // 각 작업 배포 가능일 계산
+    const daysLeft = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
     
-    // 배포
-    let result = [];
-    let count = 1;
-    for(let i = 1; i < days.length; i++) {
-        if(days[i] <= days[0]) { // 현재 기능이 기준 기능보다 늦게 끝나지 않으면(같이 배포 가능)
-            count++; // 카운트
-        } else { // 현재 기능이 기준 기능보다 늦게 끝날 경우
-            result.push(count); // 지금까지 모은 기능 배포
-            days[0] = days[i]; // 기준일 업데이트
-            count = 1; // 새 배포 시작
+    let count = 0; // 배포될 작업의 수 카운트
+    let maxDay = daysLeft[0]; // 현재 배포될 작업 중 가장 늦게 배포될 작업의 가능일
+    
+    for(let i = 0; i < n; i++) {
+        if(daysLeft[i] <= maxDay) { // 배포 가능일이 가장 늦은 배포일보다 빠르면
+            count++;
+        } else { // 배포 예정일이 기준 배포일보다 느리면
+            answer.push(count);
+            count = 1;
+            maxDay = daysLeft[i];
         }
     }
-    
-    result.push(count); // 마지막 배포
-    return result;
+    answer.push(count);
+    return answer;
 }
