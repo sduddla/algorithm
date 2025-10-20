11b23c77cbf74624413c4953f1c0ab59c7709fd8
sy <sduddla921@gmail.com>
2025-10-20 17:18:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.97 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,13 +1,27 @@
 function solution(participant, completion) {
-    // participant 마라톤에 참여한 선수들의 이름이 담긴 배열
-    // completion 완주한 선수들의 이름이 담긴 배열
+    // 해시 테이블 생성
+    const obj = {};
     
-    participant.sort();
-    completion.sort();
-    
-    for(let i = 0; i < participant.length; i++) {
-        if(participant[i] !== completion[i]) {
-            return participant[i];
+    // 참가자들 이름 해시 테이블에 추가
+    for(const p of participant) {
+        if(obj[p]) {
+            obj[p] += 1;
+        } else {
+            obj[p] = 1;
         }
     }
+    
+    // 완주한 선수들의 이름을 키로 하는 값 1씩 감소
+    for(const c of completion) {
+        obj[c] -= 1;
+    }
+    
+    // 해시 테이블에 남아 있는 선수가 완주하지 못한 선수
+    for(const key in obj) {
+        if(obj[key] > 0) {
+            return key;
+        }
+    }
+    
+
 }
