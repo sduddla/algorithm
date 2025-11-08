4d0b74e45e9acfd45ff83ce87e226fa06a232c9b
sy <sduddla921@gmail.com>
2025-11-08 17:34:51 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,7 +1,6 @@
 function solution(s) {
-    const arr = s.split(' ').map(Number).sort((a,b) => a-b);
+    const arr = s.split(' ').map(Number).sort((a, b) => a - b);
     const min = arr[0];
     const max = arr[arr.length - 1];
     return `${min} ${max}`;
-    
 }
