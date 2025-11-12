95cc010a510c41e1465ba25ea5430d5e007225db
sy <sduddla921@gmail.com>
2025-11-12 14:46:06 +0900

    [level 1] Title: 폰켓몬, Time: 0.56 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,13 @@
 function solution(nums) {
-    const types = new Set(nums); // 중복 제거
-    const take = nums.length / 2; // 가져갈 수 있는 마리 수
-    return Math.min(types.size, take);
+    // N 마리 포켓몬 중에서 N/2 만큼 선택 가능
+    // 동일한 종류 포켓몬 뽑을 시, 하나로 처리
     
+    // 전체 포켓몬 종류, 중복 제거
+    const types = new Set(nums);
+    
+    // 가져갈 수 있는 마리 수
+    const takes = nums.length / 2;
+    
+    // 전체 포켓몬 개수, 가져갈 수 있는 개수
+    return Math.min(types.size, takes);
 }
