49e6961ed287c5688b290b13969ed85e074a7a43
sy <sduddla921@gmail.com>
2025-11-14 15:33:12 +0900

    [level 1] Title: 실패율, Time: 1340.85 ms, Memory: 42.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,39 +1,18 @@
 function solution(N, stages) {
-    // N 스테이지 개수
-    // stages 게임을 이용하는 사용자가 현재 멈춰있는 스테이지 번호가 담긴 배열
-    // 실패율
-    // 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
+    // N 전체 스테이지 개수
+    // stages 게임 이용하는 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열
+    // 실패율, 스테이지에 도달했으나 아직 클리어하지 못한 플레이어 수 / 스테이지에 도달한 플레이어 수
     
-    // 스테이지 별 도전자 수
-    const challenger = new Array(N + 2).fill(0);
-    
-    for(const stage of stages) {
-        challenger[stage] += 1;
-    }
-    
-    // 스테이지 별 실패자 수 계산
-    const fails = {};
-    
-    // 스테이지에 도달한 플레이어 수
+    const result = []; // 실패율이 담긴 배열
     let total = stages.length;
     
-    // 각 스테이지 순회하며, 실패율 계산
+    // 스테이지 순회
     for(let i = 1; i <= N; i++) {
-        if(challenger[i] === 0) { // 도전한 사람이 없는 경우
-            fails[i] = 0;
-            continue;
-        }
-        
-        // 실패율 계산
-        fails[i] = challenger[i] / total;
-        
-        // 다음 스테이지 실패율 구하기 위해 현재 스테이지 인원 뺌
-        total -= challenger[i];
+        let current = stages.filter(stage => stage === i).length;
+        result.push([i, current / total])
+        total = total - current
     }
+    result.sort((a, b) => b[1] - a[1]);
     
-    // 실패율이 높은 스테이지부터 내림차순으로 정렬
-    const result = Object.entries(fails).sort((a, b) => b[1] - a[1]);
-    
-    // 스테이지 번호만 반환
-    return result.map((item) => Number(item[0]));
+    return result.map((item) => item[0]);
 }
