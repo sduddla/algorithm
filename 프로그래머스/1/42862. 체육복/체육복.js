1e7ceea83a6465938ef0d64cfd98c2786547b3a8
sy <sduddla921@gmail.com>
2025-11-16 18:51:15 +0900

    [level 1] Title: 체육복, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,25 +1,28 @@
 function solution(n, lost, reserve) {
-    // 정렬
+    // 바로 앞번호, 바로 뒷번호 체육복 빌려주기 가능
+    // 여벌 체육복 있는 학생만 다른 학생에게 빌려주기 가능
+    
     lost.sort((a, b) => a - b);
     reserve.sort((a, b) => a - b);
     
-    // 여벌 가져왔지만 도난당한 학생
-    let realLost = lost.filter(l => !(reserve.includes(l)));
-    let realReserve = reserve.filter(r => !(lost.includes(r)));
+    // 여벌 가져왔지만 도난당한 학생은 빌려줄 수 없으니 제외
+    let realLost = lost.filter((l) => !(reserve.includes(l)));
     
-    // 수업 들을 수 있는 학생
-    let cnt = n - realLost.length;
+    // 빌려줄 수 있는 학생 목록
+    let realReserve = reserve.filter((r) => !(lost.includes(r)));
     
-    // 체육복 빌려주기
+    let student = n - realLost.length; // 수업 들을 수 있는 학생
+    
     for(let num of realLost) {
-        // 앞 번호 학생이 빌려줌
+        // 앞 번호 학생이 빌려줄 경우
         if(realReserve.includes(num - 1)) {
-            realReserve = realReserve.filter(r => r !== num - 1); // 빌려주고 제거
-            cnt++;
-        } else if(realReserve.includes(num + 1)) {
-            realReserve = realReserve.filter(r => r !== num + 1);
-            cnt++;
+            realReserve = realReserve.filter((r) => r !== num - 1); // 빌려주고 제거
+            student++;
+        } else if(realReserve.includes(num + 1)) { // 뒷 번호 학생이 빌려줄 경우
+            realReserve = realReserve.filter((r) => r !== num + 1);
+            student++;
         }
     }
-    return cnt;
+    
+    return student;
 }
