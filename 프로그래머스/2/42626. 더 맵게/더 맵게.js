82599867d8d677c62ce0585328a7971b83d2180e
sy <sduddla921@gmail.com>
2025-11-20 14:30:02 +0900

    [level 2] Title: 더 맵게, Time: 730.09 ms, Memory: 99.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.js
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -5,25 +5,27 @@ class MinHeap {
     
     // 값 추가
     push(value) {
-        this.heap.push(value); // 끝에 넣기
+        this.heap.push(value); // 끝에 추가하기
         let index = this.heap.length - 1;
         
-        while(index > 0) { // 부모랑 비교해서 더 작으면 계속 위로 올리기
+        // 부모랑 비교했을 때 더 작으면 위로 계속 올리기
+        while(index > 0) {
             let parentIndex = Math.floor((index - 1) / 2);
             if(this.heap[parentIndex] <= this.heap[index]) break;
             
-            // 교환
+            // 자리 교환
             [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
             index = parentIndex;
         }
     }
     
     // 최소값 꺼내기
+    // 배열 마지막 값 루트로 올려서 아래로 내려가며 정렬
     pop() {
         if(this.heap.length === 0) return null;
         if(this.heap.length === 1) return this.heap.pop();
         
-        let min = this.heap[0]; // 루트값 (제일 작은 값)
+        let min = this.heap[0]; // 루트값, 가장 작은 값
         this.heap[0] = this.heap.pop(); // 마지막 원소 루트로
         
         let index = 0;
@@ -32,19 +34,14 @@ class MinHeap {
             let right = index * 2 + 2;
             let small = index;
             
-            if(left < this.heap.length && this.heap[left] < this.heap[small]) {
-                small = left;
-            }
-            if(right < this.heap.length && this.heap[right] < this.heap[small]) {
-                small = right;
-            }
+            if(left < this.heap.length && this.heap[left] < this.heap[small]) small = left;
+            if(right < this.heap.length && this.heap[right] < this.heap[small]) small = right;
             if(small === index) break;
             
-            // 교환
             [this.heap[index], this.heap[small]] = [this.heap[small], this.heap[index]];
+            
             index = small;
         }
-        
         return min;
     }
     
@@ -60,13 +57,13 @@ class MinHeap {
 }
 
 function solution(scoville, K) {
-    // 모든 음식의 스코빌 지수 k 이상으로
-    // 섞은 횟수 최소, 불가능 -1
+    // 모든 음식의 스코빌 지수가 K 이상 될 때까지 반복하여 섞기
     
     let heap = new MinHeap();
     for(let s of scoville) heap.push(s);
     
     let count = 0;
+    
     while(heap.size() > 1 && heap.peek() < K) {
         let first = heap.pop();
         let second = heap.pop();
