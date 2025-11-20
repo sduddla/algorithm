844b6710bc7e13fe7b26082f74c8f4eade47d4e1
sy <sduddla921@gmail.com>
2025-11-20 14:02:56 +0900

    [level 1] Title: 달리기 경주, Time: 158.77 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,23 +1,26 @@
 function solution(players, callings) {
+    // 저장할 객체 만들기
+    // 그 객체에 players의 이름과 인덱스 저장
+    // callings 순회하면서 불린 선수의 위치 찾고 그 불린 선수의 앞사람 이름 찾기
+    // players 배열에서 자리 바꾸기
+    // 객체에서도 자리 바꿔주기
+    
     let save = {};
+    
     for(let i = 0; i < players.length; i++) {
-        save[players[i]] = i; // 선수 이름과 위치 저장(mumu:0, soe:1)
+        save[players[i]] = i;
     }
     
-    for(let name of callings) {
-        let index = save[name]; // 불린 선수 현재 위치 찾기
+    for(name of callings) {
+        let index = save[name]; // 불린 선수 위치
+        let previousName = players[index - 1]; // 불린 선수의 앞사람 이름
         
-        let prev = players[index - 1]; // 불린 선수 앞 사람 이름
-        
-        // players 배열에서 자리 바꾸기
+        // 자리 위치 바꾸기
         [players[index - 1], players[index]] = [players[index], players[index - 1]];
-            
-        // 위치 바꾸기  
-        save[name] = index  - 1;    
-        save[prev] = index;    
+        
+        save[name] = index - 1;
+        save[previousName] = index; 
+        
     }
-    
-     return players;
+    return players;
 }
-    
-   
