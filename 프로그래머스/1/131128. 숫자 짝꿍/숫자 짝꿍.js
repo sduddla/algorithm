2ef3637919a7c7b295e4ed705163833cf2edc03e
sy <sduddla921@gmail.com>
2025-12-04 18:01:13 +0900

    [level 1] Title: 숫자 짝꿍, Time: 1669.60 ms, Memory: 203 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,26 +1,23 @@
 function solution(X, Y) {
-    // 0~9 각 숫자 개수 저장할 배열
-    const cntX = Array(10).fill(0);
-    const cntY = Array(10).fill(0);
+    const result = [];
     
-    // X 문자열에 등장하는 각 숫자 개수 카운트
-    for(let nCnt of X) {
-        cntX[nCnt]++;
+    // Y 개수 세기
+    const countY = {};
+    for(let ch of Y) {
+        countY[ch] = (countY[ch] || 0) + 1;
     }
     
-    // Y 문자열에 등장하는 각 숫자 개수 카운트
-    for(let nCnt of Y) {
-        cntY[nCnt]++;
+    // X에서 문자 하나씩 가져오기
+    for(let ch of X) {
+        if(countY[ch] > 0) {
+            result.push(ch);
+            countY[ch]--;
+        }
     }
     
-    let result = '';
+    if(result.length === 0) return "-1";
+    result.sort((a, b) => b - a);
     
-    for(let i = 9; i >= 0; i--) {
-        const common = Math.min(cntX[i], cntY[i]); // 최소 개수
-        result += i.toString().repeat(common); // 해당 숫자 반복해서 추가
-    }
-    
-    if(result === '') return '-1'; // 공통 숫자 없을 경우
-    if(result[0] === '0') return '0'; // 0만 여러 개 있을 때 0 하나만 반환
-    return result;
+    if(result[0] === '0') return '0';
+    return result.join('');
 }
