975c48cc6ec5d4a4db4d49b3ffedca04e1630c35
sy <sduddla921@gmail.com>
2026-02-12 01:45:34 +0900

    [Bronze V] Title: A+B - 7, Time: 100 ms, Memory: 9520 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().split('\n');
+
+const T = Number(input[0]);
+
+for(let i = 1; i <= T; i++) {
+    const [A, B] = input[i].split(' ').map(Number);
+    console.log(`Case #${i}: ${A + B}`);
+}
