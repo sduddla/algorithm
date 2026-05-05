79d5680ba12032cfdf27c8f063b3eac55588478f
sy <sduddla921@gmail.com>
2026-05-05 23:11:16 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,11 @@
+function solution(price, money, count) {
+    let sum = 0;
+    
+    for(let i = 1; i <= count; i++) {
+        sum += price * i;
+    }
+    
+    let result = sum - money;
+    return result > 0 ? result : 0;
+    
+}
